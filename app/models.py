9949206728bbb95928e9d2e15121d4c09fa96004
bkmd11 <bkendall11@gmail.com>
2021-07-09 17:32:03 -0400

    building out my models

--- app/models.py
+++ app/models.py
@@ -7,5 +7,6 @@ class Climb(db.Model):
     id = Column(Integer, primary_key=True)
     climb_name = Column(String(20), index=True)
     location = Column(String(20), index=True)
+    picture_path = Column(String(20), index=True)
     description = Column(String(250), index=True)
     getting_there = Column(String(250), index=True)
